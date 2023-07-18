e7110a045bc0c08ab3a893399989bda0deb2406f
nafizfouad <nafizfouad@gmail.com>
2023-07-18 19:49:06 +0600

    Initial commit

---
+++ app/src/main/java/com/example/currencyconverter/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.currencyconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+Button btnclr, btncalc;
+
+EditText exitInput;
+
+RadioButton r1,r2,r3;
+
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate((savedInstanceState);
+        btncalc = findViewById(R.id.btncalc);
+    }
+}
